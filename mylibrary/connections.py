89da4a0955881bb2af48178ff908c9e196e119af
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-08-06 11:58:33 +0100

    updates after data delete

--- mylibrary/connections.py
+++ mylibrary/connections.py
@@ -1,11 +1,11 @@
 # Now write out to postgres
 import psycopg2
-con_string = "host='localhost' dbname='postgres' user='postgres' password=''"
+con_string = "host='localhost' dbname='postgres' user='robinlinacre' password=''"
 conn = psycopg2.connect(con_string)
 cursor = conn.cursor()
 
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://postgres:@localhost:5432/postgres') 
+engine = create_engine('postgresql://robinlinacre:@localhost:5432/postgres') 
 
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
